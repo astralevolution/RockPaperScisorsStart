599f2299f19dddb50b9abab2e8f96438fadcc8ed
astralevolution <120140754+astralevolution@users.noreply.github.com>
2024-04-11 20:30:24 +0000

    game

--- game.py
+++ game.py
@@ -1,45 +1,38 @@
 import random
 
+def game():
+    player_choice = input('Choose rock, paper, or scissors: ').lower()
+    computer_number = random.randint(1, 3)
+    computer_choice = ''
 
-player_choice = input('Do you want rock, paper, or scisors?').lower()
-computer_number = random.randint(1,3)
-computer_choice = ''
+    if computer_number == 1:
+        computer_choice = 'rock'
+    elif computer_number == 2:
+        computer_choice = 'paper'
+    elif computer_number == 3:
+        computer_choice = 'scissors'
 
-# Computer number is either one, two or three
-
-if computer_number == 1:
-    # Make computer_choice rock
-    # Delete pass when you add some code
-    pass
-
-elif computer_number == 2:
-    # Make computer_choice paper
-    # Delete pass when you add some code
-    pass
-
-elif computer_number == 3:
-    # Make computer_choice scisors
-    # Delete pass when you add some code
-    pass
-
-winner = ''
-if player_choice == 'rock':
-    # Decide who won and store it in winner
-    # Delete pass when you add some code
-    pass
-elif player_choice == 'paper':
-    # Decide who won and store it in winner
-    # Delete pass when you add some code
-    pass
-elif player_choice == 'scissors':
-    # Decide who won and store it in winner
-    # Delete pass when you add some code
-    pass
-
-
-
-
-print('You chose: ' + ' ' + player_choice)
-print('The computer chose' + ' ' + computer_choice)
-print('The ' + winner + ' won!')
-
+    winner = ''
+    if player_choice in ['rock', 'paper', 'scissors']:
+        if (player_choice == 'rock' and computer_choice == 'scissors') or \
+           (player_choice == 'paper' and computer_choice == 'rock') or \
+           (player_choice == 'scissors' and computer_choice == 'paper'):
+            winner = 'You'
+        elif player_choice == computer_choice:
+            winner = "It's a tie!"
+        else:
+            winner = 'The computer'
+        print()
+        print('You chose ' + player_choice + '.')
+        print('The computer chose ' + computer_choice + '.')
+        print()
+        print(winner + ' won!')
+    else:
+        print("You did not choose rock, paper, or scissors, try again.")
+        game()
+        return
+    print("Do you want to play again? (Y/N)")
+    playagain = input()
+    if(playagain.lower() == 'y'):
+        game()
+game()
